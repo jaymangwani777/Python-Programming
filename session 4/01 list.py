996b82839d46f771f2334c4c56009e24f046c1e9
jaymangwani777 <160834087+jaymangwani777@users.noreply.github.com>
2024-10-03 16:21:27 +0530

    Python-Programming

---
+++ session 4/01 list.py
@@ -0,0 +1,10 @@
+friends=["Apple","orange",5,345.0008,"piyush","DAD"]
+
+print(friends[5])
+
+# strings are immuatable that means you cannot change a string
+# on the other way lists are mutable you can change them
+# like in this case
+
+friends[5]="MOM"
+print(friends[5])
